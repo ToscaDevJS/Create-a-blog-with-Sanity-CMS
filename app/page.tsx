ae3ba6bd8a3d36ee0cf3799688c44e460e0fd6e8
ToscaDevJS <orly27esp@gmail.com>
2024-03-12 04:46:30 +0100

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
 export default function Home() {
   return (
     <main>
-      <header className="border text-white p-5 text-2xl flex justify-center items-center h-24">
+      <header className="border text-white p-5 text-2xl flex justify-center items-center">
         <ul className="flex space-x-4">
           <li>
             <Link href="/blog" className="text-gray-500 underline">
@@ -13,7 +13,7 @@ export default function Home() {
             <ArrowUpRightIcon className="h-8 w-8 text-gray-500 inline-block" />
           </li>
           <li>
-            <Link href="/Comment" className=" text-gray-500 underline">
+            <Link href="/notasapi" className=" text-gray-500 underline">
               NOTAS API
             </Link>
             <ArrowUpRightIcon className="h-8 w-8 text-gray-500 inline-block" />
